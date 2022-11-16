ce398cd1f3e3d35f9985c79d0169c8c5617c9d87
Stephen Townsend <26904641+townsste@users.noreply.github.com>
2022-11-15 22:55:54 -0800

    Update mergesrt.sh

--- mergesrt.sh
+++ mergesrt.sh
@@ -116,19 +116,13 @@ process() {
         return
     fi
     echo -e "\e[1;32mSTARTING MERGE\e[m"
-    MERGE_FILE=$FILE_NAME'.merge'
-    FILE=$(echo "$FILE_NAME" | rev | cut -d'/' -f1 | rev)
-    
-    CURR_SUB_COUNT="$(mkvmerge --identify "$FILE_NAME" | grep -c 'subtitle')" # Count the number of subs in the pre processed file
-    echo "File Name: $FILE_NAME"
-    mkvmerge --identify "$FILE_NAME"
+    MERGE_FILE=$FILE_NAME'.merge'    
+    CURR_SUB_COUNT="$(mkvmerge --identify "$VIDEO_FILE" | grep -c 'subtitle')" # Count the number of subs in the pre processed file
     echo "SUB COUNT: $CURR_SUB_COUNT"
     merge "$MERGE_FILE" "$VIDEO_FILE" "$IMPORT_FILE" "$EXT" "$TYPE" "$LANG"
     # When doing large batches sometimes the merge does not seem to work correctly.
     # this is used to keep running the merge untill the file has detected a subtitle.
     NEXT_SUB_COUNT="$(mkvmerge --identify "$MERGE_FILE" | grep -c 'subtitle')" # Count the number of subs in the post processed file
-    echo "Merge Name: $MERGE_FILE"
-    mkvmerge --identify "$MERGE_FILE"
     echo "NEXT COUNT: $NEXT_SUB_COUNT"
     while ("$CURR_SUB_COUNT" -le "$NEXT_SUB_COUNT") do
         echo -e "\e[0;31mSubtitle is missing from merge file.  Rerunning merge\e[m"
