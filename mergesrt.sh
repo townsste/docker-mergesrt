bdef84d8090065de810e0415cb7f9ef6f800f038
Stephen Townsend <26904641+townsste@users.noreply.github.com>
2022-11-22 01:21:05 -0800

    Update mergesrt.sh
    Bug where forced would not trigger after lang.  Just checking for ext to trigger now.

--- mergesrt.sh
+++ mergesrt.sh
@@ -150,13 +150,13 @@ process() {
 }
 
 # LOOK FOR FILES ON STARTUP -------------------------------------------------------------
-find "$DATA_DIR" -type f -name "*.???*.??.srt" -o -name "*.???*.???.srt" -o -name "*.idx" -o -name "*.ass"|
+find "$DATA_DIR" -type f -name "*.srt" -o -name "*.idx" -o -name "*.ass"|
     while read file; do
         process "$file"
     done
     
 # MONITOR FOR NEW FILES IN DIR ----------------------------------------------------------
-inotifywait -m -r $DATA_DIR -e create -e moved_to --include '.*\.([a-z]{2,3}\.srt|idx|ass)$' --format '%w%f' |
+inotifywait -m -r $DATA_DIR -e create -e moved_to --include '(.srt|.idx|.ass)' --format '%w%f' |
     while read file; do
         echo "The file '$file' was created/moved"
         process "$file"
